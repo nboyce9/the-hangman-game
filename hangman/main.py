132a461be97e928bfd172bdcdd4ba0aaae0c1b31
nboyce <boycenobukho@gmail.com>
2025-04-27 12:10:13 +0200

    added a run method to run the game, added ascii art. its now a playable game

--- hangman/main.py
+++ hangman/main.py
@@ -8,7 +8,7 @@ import random
 # def getCategory():
 #     category = input("Enter the category (e.g 1 for 'Fruits'): ")
 #     return category
-
+isGameOver = False
 
 def readFile(filename):
     with open(filename, 'r') as file:
@@ -45,6 +45,87 @@ def updateWordFromGuess(guess, word, blacked_out_word):
                     blacked_out_word[i] = word[i]
     blacked_out_word = "".join(blacked_out_word)
     return blacked_out_word
+
+def isCorrectGuess(guess, word):
+    if guess in word:
+        return True
+    else:
+        return False
+    
+def drawHangman(lives):
+    match lives:
+        case 4:
+            print("""/----\n|\n|\n|\n|\n_______""")
+        case 3:
+            print("""/----\n|   0\n|\n|\n|\n_______""")
+        case 2:
+           print("""/----\n|   0\n|  /|\\\n|\n|\n_______""") 
+        case 1:
+            print("""/----\n|   0\n|  /|\\\n|   |\n|\n_______""") 
+        case 0:
+            print("""/----\n|   0\n|  /|\\\n|   |\n|  / \\\n_______""")
+
+def gameOpening():
+    print(r"""
+            *********************************************************************
+            *                  _                                 _              *
+            *    __      _____| | ___ ___  _ __ ___   ___       | |_ ___        *
+            *    \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \      | __/ _ \       *
+            *     \ V  V /  __/ | (_| (_) | | | | | |  __/      | || (_) |      *
+            *     _\_/\_/ \___|_|\___\___/|_| |_| |_|\___|       \__\___/       *
+            *    | | | | __ _ _ __   __ _ _ __ ___   __ _ _ __                  *
+            *    | |_| |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \                 *
+            *    |  _  | (_| | | | | (_| | | | | | | (_| | | | |                *
+            *    |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|                *
+            *                       |___/                                       *
+            *********************************************************************
+            """)
+    print("\nYou have 5 lives to guess the word.")
+    print("Good luck!\n")
+
+
+def gameOver():
+    print(r"""
+            ****************************************************************************
+            *                                                                          *
+            *                                                                          *
+            *       ____                           ___                   _   _   _     *
+            *      / ___| __ _ _ __ ___   ___     / _ \__   _____ _ __  | | | | | |    *
+            *     | |  _ / _` | '_ ` _ \ / _ \   | | | \ \ / / _ \ '__| | | | | | |    *
+            *     | |_| | (_| | | | | | |  __/   | |_| |\ V /  __/ |    |_| |_| |_|    *
+            *      \____|\__,_|_| |_| |_|\___|    \___/  \_/ \___|_|    (_) (_) (_)    *
+            *                                                                          *
+            *                                                                          *
+            *                                                                          *
+            ****************************************************************************
+            """)
+    
+    
+def runGame():
+    initial_lives = 5
+    gameOpening()
+    word_list = readFile("temp_words.txt")
+    word = getWord(word_list)
+    blacked_out_word = blackOutWord(word)
+    print(f"Random word: {word}")
+    print(f"Blacked out word: {blacked_out_word}")
+    while not isGameOver:
+        guess = guessWordInput()
+        blacked_out_word = updateWordFromGuess(guess, word, blacked_out_word)
+        if isCorrectGuess(guess, word):
+            print("Congratulations! You've guessed the word!")
+            break
+        else:
+            print(f"Updated word: {blacked_out_word}")
+        initial_lives -= 1
+        drawHangman(initial_lives)
+        if initial_lives == 0:
+            gameOver()
+            print(f"The word was '{word}'")
+            break
+        print(f"Lives left: {initial_lives}")
+        print(f"Blacked out word: {blacked_out_word}")
+
     
 
 
@@ -55,12 +136,6 @@ if __name__ == "__main__":
     # level = getLevel()
     # category = getCategory()
     # print(f"Level: {level} in {category} category")
-    word_list = readFile("temp_words.txt")
-    word = getWord(word_list)
-    print(f"Random word: {word}")
-    blacked_out_word = blackOutWord(word)
-    print(f"Blacked out word: {blacked_out_word}")
-    guess = guessWordInput()
-    print(updateWordFromGuess(guess, word, blacked_out_word))
+    runGame()
 
 
