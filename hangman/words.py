4b1795dc4c9884d6cabea8f0976c1a892bf698c7
nboyce <boycenobukho@gmail.com>
2025-04-28 08:30:50 +0200

    added helper method when a user chooses a category

--- hangman/words.py
+++ hangman/words.py
@@ -6,28 +6,38 @@ def writeToFile(words):
         for word in words:
             file.write(word + "\n")
 
-def get_words():
+
+def getCategoryString( category):
+    match category:
+        case 1:
+            category = "plant"
+        case 2: 
+            category = "animal"
+        case 3:
+            category = "country"
+        case 4: 
+            category = "food"
+        case 5: 
+            category = "sport"
+    return category
+
+
+def get_words(category):
     # url = "https://www.wordgamedb.com/api/v1/words/random/"
 
-    category = 'country'
+    categ = getCategoryString(category)
     words = []
     for i in range(3, 13):
-        url = f"https://www.wordgamedb.com/api/v1/words/?category={category}&numLetters={i}"
+        url = f"https://www.wordgamedb.com/api/v1/words/?category={categ}&numLetters={i}"
 
         response = requests.get(url)
         if response.status_code == 200:
             x = response.json()
             for item in x:
-                print(item["word"])
                 words.append(item["word"])
         else:
             print("Error:", response.status_code)
             return []
+        
     writeToFile(words)
     return words
-    
-
-    
-if __name__ == "__main__":
-    words = get_words()
-    writeToFile(words)
