d0440eaf58988df3bec554051eef51b16e9bf0f8
nboyce <boycenobukho@gmail.com>
2025-04-28 08:05:40 +0200

    modified to request words from a category

--- hangman/words.py
+++ hangman/words.py
@@ -9,24 +9,25 @@ def writeToFile(words):
 def get_words():
     # url = "https://www.wordgamedb.com/api/v1/words/random/"
 
-    category = 'plant'
-    numLetters = 5
-    url = f"https://www.wordgamedb.com/api/v1/words/?category={category}&numLetters={numLetters}"
+    category = 'country'
+    words = []
+    for i in range(3, 13):
+        url = f"https://www.wordgamedb.com/api/v1/words/?category={category}&numLetters={i}"
 
-    response = requests.get(url)
-    if response.status_code == 200:
-        x = response.json()
-        words= []
-        for i in x:
-            words.append(i["word"])
-        
-    else:
-        print("Error:", response.status_code)
-        return None
+        response = requests.get(url)
+        if response.status_code == 200:
+            x = response.json()
+            for item in x:
+                print(item["word"])
+                words.append(item["word"])
+        else:
+            print("Error:", response.status_code)
+            return []
     writeToFile(words)
+    return words
     
 
     
-# if __name__ == "__main__":
-#     words = get_words()
-#     writeToFile(words)
+if __name__ == "__main__":
+    words = get_words()
+    writeToFile(words)
